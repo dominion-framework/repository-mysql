1eb3c82fd705693192d5aa165c86601e5867b3f9
Yuriy Chaikovsky <yura.chaikovsky@gmail.com>
2019-08-04 16:04:04 +0300

    🍾

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const Errors                    = require("dominion/core/errors");
+const RepositoryPrototype       = require("./repositoryPrototype");
+
+
+class Repositories {
+    static create(tableName, repositoryPrototype, repositoryDefinition) {
+        if (!tableName) {
+            throw new Errors.Fatal(`Table name can not be empty.`);
+        }
+
+        if (!repositoryDefinition) {
+            repositoryDefinition = repositoryPrototype;
+            repositoryPrototype = RepositoryPrototype;
+        }
+
+        const className = "Repository" + tableName[0].toUpperCase() + tableName.slice(1);
+        const Repository = {[className] : class extends repositoryPrototype {
+              constructor() {
+                  super();
+                  this.__table__ = tableName;
+              }
+        }}[className];
+
+        Object.assign(Repository.prototype, repositoryDefinition || {});
+
+        return new Repository();
+    }
+}
+
+
+module.exports = Repositories;
